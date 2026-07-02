2394662c29a9202853f2691c07794a1faea2cb86
kkyo1130 <mary3254@naver.com>
2026-07-02 19:17:18 +0900

    reset

---
+++ src/main/java/likelion14th/lte/user/dto/response/UserProfileResponse.java
@@ -0,0 +1,22 @@
+package likelion14th.lte.user.dto.response;
+
+import likelion14th.lte.user.entity.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserProfileResponse {
+    private String Username;
+    private String profileImageUrl;
+    private String introduction;
+
+    public static UserProfileResponse from (User user) {
+        return new UserProfileResponse(
+                user.getUsername() + "#" + user.getUserTag(),
+                user.getProfileImage(),
+                user.getIntroduction()
+        );
+    }
+}
